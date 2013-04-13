42fffc267b3fdea07debf69352ffacd8d348c7e2
Dima Stopel <dima@stopel.org>
2013-04-13 13:07:30 +0300

    adding some content

---
+++ dima/images.pl
@@ -0,0 +1,12 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/bullet;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\bullet$">|; 
+
+1;
+
